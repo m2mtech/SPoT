e8f686972ad9ca1f825e42deaf200dead0baa6a4
m2m tech <tech@m2m.at>
2013-03-02 16:48:31 +0100

    assignment #4 task #1

--- SPoT/ViewController.m
+++ SPoT/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "FlickrFetcher.h"
 
 @interface ViewController ()
 
@@ -17,13 +18,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSLog(@"%@", [FlickrFetcher stanfordPhotos]);
 }
 
 @end
