79b0e437fe7292bb71798f5ffef194b1cba53fd9
m2m tech <tech@m2m.at>
2013-03-02 19:58:56 +0100

    assignment #4 task #5

---
+++ SPoT/ImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.h
+//  Shutterbug
+//
+//  Created by Martin Mandl on 02.03.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@property (nonatomic, strong) NSURL *imageURL;
+
+@end
---
+++ SPoT/ImageViewController.m
@@ -0,0 +1,66 @@
+//
+//  ImageViewController.m
+//  Shutterbug
+//
+//  Created by Martin Mandl on 02.03.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import "ImageViewController.h"
+
+@interface ImageViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImageView *imageView;
+
+@end
+
+@implementation ImageViewController
+
+- (void)setImageURL:(NSURL *)imageURL
+{
+    _imageURL = imageURL;
+    [self resetImage];
+}
+
+- (void)resetImage
+{
+    if (self.scrollView) {
+        self.scrollView.contentSize = CGSizeZero;
+        self.imageView.image = nil;
+        
+        NSData *imageData = [[NSData alloc] initWithContentsOfURL:self.imageURL];
+        UIImage *image = [[UIImage alloc] initWithData:imageData];
+        if (image) {
+            self.scrollView.zoomScale = 1.0;
+            self.scrollView.contentSize = image.size;
+            self.imageView.image = image;
+            self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+        }
+    }
+}
+
+- (UIImageView *)imageView
+{
+    if (!_imageView) {
+        _imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    }
+    return _imageView;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	[self.scrollView addSubview:self.imageView];
+    self.scrollView.minimumZoomScale = 0.2;
+    self.scrollView.maximumZoomScale = 5.0;
+    self.scrollView.delegate = self;
+    [self resetImage];
+}
+
+@end
